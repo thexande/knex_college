d7e74aec76e98991cb73f22af76256e0b213aa88
Alex <amurphy345@gmail.com>
2016-07-06 10:47:08 -0600

    knex working great

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+
+var cars = () => {
+  return knex('cars')
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  cars().then((resp) => {
+    res.send(resp)
+  })
 });
 
 module.exports = router;
